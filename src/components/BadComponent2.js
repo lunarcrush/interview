b660ee185491fffcaa3fe5f67d9ed1c32ff103c7
Dan Williams <pleaseshutup@gmail.com>
2023-02-05 12:28:03 +0000

    ch(Examples): updated some example components

--- src/components/BadComponent2.js
+++ src/components/BadComponent2.js
@@ -4,6 +4,22 @@ import { View, Text } from "react-native";
 export default function BadComponent2(props) {
   return (
     <View>
+      <View>
+        <Text>Favorite colors</Text>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <View>
+          <View>
+            <Text>Red</Text>
+          </View>
+          <View>
+            <Text>Orange</Text>
+          </View>
+          <View>
+            <Text>Blue</Text>
+          </View>
+        </View>
+      </View>
       <Text>Cameron i welcome your ideas here</Text>
     </View>
   );
--- src/components/BadComponent3.js
+++ src/components/BadComponent3.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function BadComponent3(props) {
   return (
     <View>
-      <Text>Emilio i welcome your ideas here</Text>
+      <Text>Placeholder</Text>
     </View>
   );
 }
